a68efbb9901a92eb5a36ddc01208da2e76ee9835
Paul Keller <paulmkeller@hotmail.com>
2015-11-30 23:02:00 +0800

    Initial Commit

---
+++ Booze Tracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Booze Tracker
+//
+//  Created by Paul Keller on 30/11/2015.
+//  Copyright © 2015 Planet K Games. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
