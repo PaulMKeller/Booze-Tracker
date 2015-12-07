218f24aeb2eadadaabf714eae4dfa1b44ba5ed6e
Paul Keller <paulmkeller@hotmail.com>
2015-12-08 00:07:55 +0800

    Saving Progress

--- Booze Tracker/HomeViewController.swift
+++ Booze Tracker/HomeViewController.swift
@@ -7,17 +7,43 @@
 //
 
 import UIKit
+import iAd
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, ADBannerViewDelegate {
 
+    var bannerView: ADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.canDisplayBannerAds = true;
+        
+        bannerView = ADBannerView(adType: .Banner)
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        bannerView.delegate = self
+        bannerView.hidden = true
+        view.addSubview(bannerView)
+        
+        //let viewsDictionary = ["bannerView":bannerView]
+        /*
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        */
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        bannerView.hidden = false
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        bannerView.hidden = true
     }
 
 
